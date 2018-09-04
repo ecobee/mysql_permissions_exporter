11d3d2059d3fc314a8d909515c6d8c09ce5d2343
Victor Melo <victor.m@ecobee.com>
2018-09-04 16:45:41 -0400

    Updated the query to also select data from the mysql.user table in addition to the mysql.db table

--- mysql_permissions_exporter.py
+++ mysql_permissions_exporter.py
@@ -34,8 +34,64 @@ class Configuration:
 class MySQLUserInformation:
  
   def __init__(self, db):
+
+    # mysql.db stores permissions applied to individual databases, eg: GRANT SELECT ON ecobee.* TO 'user'@'host';
+    # mysql.user stores permissions applied to all databases, eg: GRANT SELECT ON *.* TO 'user'@'host';
     with db.cursor() as cursor:
-      cursor.execute("""SELECT * FROM db""")
+      cursor.execute(
+        """
+        SELECT * FROM (
+          SELECT 	
+            host,
+            Db,
+            User,
+            Select_priv,
+            Insert_priv,
+            Update_priv,
+            Delete_priv,
+            Create_priv,
+            Drop_priv,
+            Grant_priv,
+            References_priv,
+            Index_priv,
+            Alter_priv,
+            Create_tmp_table_priv,
+            Lock_tables_priv,
+            Create_view_priv,
+            Show_view_priv,
+            Create_routine_priv,
+            Alter_routine_priv,
+            Execute_priv,
+            Event_priv,
+            Trigger_priv
+          FROM mysql.db) as db 
+        UNION 
+        SELECT * FROM (	
+          SELECT 	
+            host,
+            '*' as 'Db',
+            User,
+            Select_priv,
+            Insert_priv,
+            Update_priv,
+            Delete_priv,
+            Create_priv,
+            Drop_priv,
+            Grant_priv,
+            References_priv,
+            Index_priv,
+            Alter_priv,
+            Create_tmp_table_priv,
+            Lock_tables_priv,
+            Create_view_priv,
+            Show_view_priv,
+            Create_routine_priv,
+            Alter_routine_priv,
+            Execute_priv,
+            Event_priv,
+            Trigger_priv
+          FROM mysql.user) as user;
+        """)
 
     self.result_set = cursor.fetchall()
     cursor.close()
